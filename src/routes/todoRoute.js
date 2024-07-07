9626bb2ec94269f75a88a47c7ea452d55ac0ee48
RAKESH GOWDA <rakeshgowda495758@gmail.com>
2024-07-07 13:54:21 +0530

    commit5

--- src/routes/todoRoute.js
+++ src/routes/todoRoute.js
@@ -7,6 +7,6 @@ todoRoute.get("/gettodo",getTodo)
 
 todoRoute.post("/addtodo",addTodo);
 
-todoRoute.patch("updatetodo",updateTodo);
+todoRoute.patch("/updatetodo/:id",updateTodo);
 
-todoRoute.delete("/deletetodo",deleteTodo);
+todoRoute.delete("/deletetodo/:id",deleteTodo);
